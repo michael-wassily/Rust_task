8f9997254d9e9fa7f98d8540d0cef02d0655867c
Michael Ashraf <maykelsawiris@gmail.com>
2024-12-21 05:06:46 +0200

    adding main and dependencies

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use embedded_recruitment_task::server::Server;
+use log::{info,error};
+use std::io;
+
+fn main()->io::Result<()>{
+    //initialize logger
+    env_logger::Builder::new()
+        .parse_filters("info")
+        .init();
+
+    //create server
+    let server=Server::new("localhost:8080")?;
+    info!("server starting on localhost:8080");
+
+    server.run()
+}
